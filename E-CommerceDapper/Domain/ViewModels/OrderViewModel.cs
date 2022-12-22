dd9a2e0a0c77dfff0b357b5addbd084f2141960b
ilkin <ilkin@MY_ASUS>
2022-12-22 23:10:23 +0400

    Add project files.

---
+++ E-CommerceDapper/Domain/ViewModels/OrderViewModel.cs
@@ -0,0 +1,70 @@
+﻿using E_CommerceDapper.DataAccess.Entites;
+using E_CommerceDapper.Domain.Commands;
+using E_CommerceDapper.Domain.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace E_CommerceDapper.Domain.ViewModels
+{
+    public class OrderViewModel:BaseViewModel
+    {
+
+		private TextBox q;
+
+		public TextBox Q
+		{
+			get { return q; }
+			set { q = value; OnPropertyChanged(); }
+		}
+
+
+		private OrderDetails order;
+
+		public OrderDetails Order
+        {
+			get { return order; }
+			set { order = value; OnPropertyChanged(); }
+		}
+
+		
+
+		public RelayCommand	SubmitCommand { get; set; }
+		public OrderViewModel(Product product)
+		{
+			Order = new OrderDetails();
+
+			//var p = new Product();
+			//var viewm = new ProductInfoViewModel(p);
+			//Order.UnitPrice = viewm.Product.UnitPrice;
+			//Order.Quantity = viewm.Product.UnitsInStock;
+			Order.UnitPrice = product.UnitPrice;
+			Order.Quantity = product.UnitsInStock;
+
+			//Order.Quantity = product.UnitsInStock;
+			SubmitCommand = new RelayCommand((o) =>
+			{
+				//App.DB.OrderDetailsRepository.Add(Order);
+
+				//short textboxquantity = short.Parse(Q.Text);
+
+				MessageBox.Show("Hello");
+
+				App.DB.OrderDetailsRepository.Add(Order);
+
+				var vm = new OrderDetailsViewModel();
+				var view = new OrderDetailsWindow();
+				view.DataContext = vm;
+
+				view.Show();
+
+
+			});
+		}
+
+	}
+}
